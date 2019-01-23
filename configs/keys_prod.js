657f0102ce80e6e93c36fa02e2c50a8efdd6988f
Arnold <arnoldleung1112@gmail.com>
2019-01-23 15:22:36 +0800

    adding production keys

--- configs/keys_prod.js
+++ configs/keys_prod.js
@@ -2,6 +2,8 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey:process.env.SECRET_OR_KEY,
     my_privkey:process.env.PRIVATE_KEY,
-    infura_api_key:process.env.INFURA_KEY
+    infura_api_key:process.env.INFURA_KEY,
+    contractAddress:process.env.CONTRACT_ADDRESS,
+    fromAddress:process.env.MAIN_ADDRESS
 }
 
